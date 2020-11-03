22b4d445bb148b1b83584880c767e233b8baa877
opo2000tw <opo2000tw@mail.fcu.edu.tw>
2020-11-03 15:57:21 +0800

    Add print

--- f723/Core/Src/usart.c
+++ f723/Core/Src/usart.c
@@ -369,7 +369,48 @@ void HAL_UART_MspDeInit(UART_HandleTypeDef* uartHandle)
 }
 
 /* USER CODE BEGIN 1 */
+// #ifdef __GNUC__
+//   /* With GCC/RAISONANCE, small printf (option LD Linker->Libraries->Small printf
+//      set to 'Yes') calls __io_putchar() */
+//   #define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+// #else
+//   #define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+// #endif /* __GNUC__ */
+// /**
+//   * @brief  Retargets the C library printf function to the USART.
+//   * @param  None
+//   * @retval None
+//   */
+// PUTCHAR_PROTOTYPE
+// {
+//   /* Place your implementation of fputc here */
+//   /* e.g. write a character to the EVAL_COM1 and Loop until the end of transmission */
+//   HAL_UART_Transmit(&USARTx, (uint8_t *)&ch, 1, 0xFFFF);
 
+//   return ch;
+// }
+
+int _write(int fd, char *ptr, int len)
+{
+    int i = 0;
+    /*
+     * write "len" of char from "ptr" to file id "fd"
+     * Return number of char written.
+     *
+    * Only work for STDOUT, STDIN, and STDERR
+     */
+    if (fd > 2)
+    {
+        return -1;
+    }
+    while (*ptr && (i < len))
+    {
+        uart_msg_send(ptr);
+        i++;
+        ptr++;
+    }
+    return i;
+}
 /* USER CODE END 1 */
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
